7f3f24f52fe02b634c685f105ec160753460f335
Aron Teh <aronteh.chess@gmail.com>
2024-02-21 15:16:16 +0000

    Add POST request for adding a photo

--- src/main/java/com/jetbrains/ellipsoul/photoz/clone/PhotozController.java
+++ src/main/java/com/jetbrains/ellipsoul/photoz/clone/PhotozController.java
@@ -6,20 +6,25 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 // Look for any rest controllers inside the application, if yes then create an instance of this controller
 @RestController
 public class PhotozController {
+  // This is our in memory database for now
   private Map<String, Photo> db = new HashMap<>() {{
     put("1", new Photo("1", "hello.jpg"));
   }};
 
+  // Basic request
   @GetMapping("/") // Executed by spring whenever we hit the home path with a get method
   public String hello() {
     return "Hello World!";
@@ -44,6 +49,14 @@ public class PhotozController {
   public Photo deletePhoto(@PathVariable String id) {
     Photo photo = db.remove(id);
     if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    return photo;
+  }
+
+  // Add a photo
+  @PostMapping("/photoz")
+  public Photo addPhoto(@RequestBody Photo photo) { // Convert JSON into the Photo class
+    photo.setId(UUID.randomUUID().toString()); // Generate a random UUID
+    db.put(photo.getId(), photo); // Add the photo to the database
     return photo;
   }
 }
